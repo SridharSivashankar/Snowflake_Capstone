e7f9829bec6a0c99dadeb92a00dd8a082a7ba82f
sridhar.sivashankar@accordion.com <sridhar.sivashankar@accordion.com>
2026-07-15 04:50:48 +0000

    Initial steps

---
+++ macros/external_tables/external_tables_loop.sql
@@ -0,0 +1,26 @@
+{% macro external_table_creation () %}
+{%set folders = [
+    'customer_data',
+    'product_data',
+    'supplier_data',
+    'employee_data',
+    'orders_data',
+    'campaign_data',
+    'store_data'
+] %}
+{% for folder in folders%}
+    {% set table_name = 'ext_'~ folder.replace('_data', '')%}
+    {% set sql %}
+    CREATE OR REPLACE EXTERNAL TABLE
+    {{ target.database}}.{{target.schema}}.{{table_name}}
+    (Col1 VARIANT as(VALUE))
+    LOCATION = @CT_SRIDHAR_SIVASHANKAR_DB.CAPSTONEPROJECT_23613.Azure_stage/Capstone_Project_Data/{{folder}}
+    FILE_FORMAT = (TYPE = JSON
+                    STRIP_OUTER_ARRAY = TRUE)
+    AUTO_REFRESH = FALSE;
+    {% endset %}
+
+    {{run_query(sql)}}
+{% endfor%}
+
+{% endmacro%}
